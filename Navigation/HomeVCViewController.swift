da3710ac802154544c5dbefd277814534fa9d4c5
Bolebo67 <bolebo7@gmail.com>
2023-05-19 16:15:26 +0200

    first commit

---
+++ Navigation/HomeVCViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeVCViewController.swift
+//  Navigation
+//
+//  Created by DA MAC M1 114 on 2023/05/19.
+//
+
+import UIKit
+
+class HomeVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
